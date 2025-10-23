d4ca749a77011b14a3de62dec93aa34848cc1753
Marco Germani <info@marcogermani.it>
2025-10-23 09:06:05 +0200

    Optimized widget

--- src/Filament/Widgets/UsersWidget.php
+++ src/Filament/Widgets/UsersWidget.php
@@ -24,36 +24,33 @@ class UsersWidget extends BaseWidget
     {
         $widgets = [];
 
-        $tag = config('filament-users.resource.class')::ADMIN_WIDGETS_DASHBOARD_TAG_KEY;
         $keyPostfix = '_count_UsersWidget';
+        $tag = config('filament-users.resource.class')::ADMIN_WIDGETS_DASHBOARD_TAG_KEY;
 
-        $usersCount = Cache::tags($tag)->rememberForever("user$keyPostfix", function () use ($keyPostfix) {
-            Log::debug("Cached key \"user$keyPostfix\": is expired fetch data from DB");
-
-            return config('filament-users.resource.model', \App\Models\User::class)::query()->count('id');
-        });
-
-        $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.widget.users'), $usersCount)
-            ->icon('heroicon-o-users');
+        $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.widget.users'),
+            Cache::tags($tag)
+                ->rememberForever("user$keyPostfix", function () use ($keyPostfix) {
+                    Log::debug("Cached key \"user$keyPostfix\": is expired fetch data from DB");
+                    return config('filament-users.resource.model', \App\Models\User::class)::query()->count('id');
+                })
+        )->icon('heroicon-o-users');
 
         if (Utils::isFilamentShieldInstalled()) {
-            $rolesCount = Cache::tags($tag)->rememberForever("role$keyPostfix", function () use ($keyPostfix) {
-                Log::debug("Cached key \"role$keyPostfix\": is expired fetch data from DB");
+            $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.resource.roles'),
+                Cache::tags($tag)
+                    ->rememberForever("role$keyPostfix", function () use ($keyPostfix) {
+                        Log::debug("Cached key \"role$keyPostfix\": is expired fetch data from DB");
+                        return Role::query()->count('id');
+                    })
+            )->icon('heroicon-o-shield-check');
 
-                return Role::query()->count('id');
-            });
-
-            $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.resource.roles'), $rolesCount)
-                ->icon('heroicon-o-shield-check');
-
-            $permissionsCount = Cache::tags($tag)->rememberForever("permission$keyPostfix", function () use ($keyPostfix) {
-                Log::debug("Cached key \"permission$keyPostfix\": is expired fetch data from DB");
-
-                return Permission::query()->count('id');
-            });
-
-            $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.resource.permissions'), $permissionsCount)
-                ->icon('heroicon-o-lock-closed');
+            $widgets[] = BaseWidget\Stat::make(__('filament-users::filament-users.resource.permissions'),
+                Cache::tags($tag)
+                    ->rememberForever("permission$keyPostfix", function () use ($keyPostfix) {
+                        Log::debug("Cached key \"permission$keyPostfix\": is expired fetch data from DB");
+                        return Permission::query()->count('id');
+                    })
+            )->icon('heroicon-o-lock-closed');
         }
 
         return $widgets;
